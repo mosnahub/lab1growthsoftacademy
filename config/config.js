ba7a58b2c3d44ba2f2f1d948c55f250dcdf0a70e
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2020-02-27 18:38:12 +0700

    .

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    httpport: 5713,
+    httpsport: 35255,
+    dbUrl: "mongodb+srv://admin:Password@cluster0-7c1z0.gcp.mongodb.net/db0?retryWrites=true&w=majority",
+    sessionSecret: "xxx",
+    passportSecret: "Growth#Since2018",
+    needReScrape: false
+};
