06165d4d2976cdcc599a247bacddb7466cbe101f
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2020-02-27 18:22:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("js");
+  res.send("Tee is GAY");
 });
 
 app.get("/foods", (req, res) => {
