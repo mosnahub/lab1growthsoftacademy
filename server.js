7f7a9c8fd59f51a8ed4b8da2c67c14fdbfa2fa54
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2020-02-27 17:58:46 +0700

    H

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hi");
 });
 
 app.get("/foods", (req, res) => {
